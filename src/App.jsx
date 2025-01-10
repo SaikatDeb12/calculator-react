e90877d2328c7a025ebdcbdbf21f546fde4a35a3
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-10 15:56:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="calculator">
+        <div className="navbar">
+          <input type="checkbox"></input>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
