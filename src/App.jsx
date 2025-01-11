0d3a244646c790d142f64335c24538c54bf09148
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-11 13:15:19 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const refToggle = useRef();
+  const refImg = useRef();
+  let flag = true;
+  const changeBtn = () => {
+    if (flag) {
+      refToggle.current.style.transform = "translate(25px, 0px)";
+      refImg.current.src = "/moon.png";
+    } else {
+      refToggle.current.style.transform = "translate(1px, 0px)";
+      refImg.current.src = "/sun.png";
+    }
+    flag = !flag;
+  };
+
   return (
     <div className="app">
       <div className="calculator">
         <div className="navbar">
-          <input type="checkbox"></input>
+          <div className="slidebar">
+            <p ref={refToggle} className="btn" onClick={changeBtn}></p>
+          </div>
+          <span>
+            <img ref={refImg} className="icon" src="/sun.png"></img>
+          </span>
         </div>
       </div>
     </div>
