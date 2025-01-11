879b1a13fbb4342c8467c1799dcd4bd36cbc786f
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-11 14:31:34 +0530

    toggle light-dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useRef } from "react";
 import "./App.css";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
 
 function App() {
   const refToggle = useRef();
   const refImg = useRef();
-  let flag = true;
+  const [flag, changeFlag] = useState(true);
   const changeBtn = () => {
     if (flag) {
       refToggle.current.style.transform = "translate(25px, 0px)";
@@ -13,19 +15,24 @@ function App() {
       refToggle.current.style.transform = "translate(1px, 0px)";
       refImg.current.src = "/sun.png";
     }
-    flag = !flag;
+
+    changeFlag(!flag);
   };
 
   return (
-    <div className="app">
+    <div className="app" data-theme={flag ? "" : "dark"}>
       <div className="calculator">
         <div className="navbar">
-          <div className="slidebar">
-            <p ref={refToggle} className="btn" onClick={changeBtn}></p>
+          <div className="slidebar" onClick={changeBtn}>
+            <p ref={refToggle} className="btn"></p>
           </div>
           <span>
             <img ref={refImg} className="icon" src="/sun.png"></img>
           </span>
+        </div>
+        <div className="main-section">
+          <Header />
+          <Footer />
         </div>
       </div>
     </div>
