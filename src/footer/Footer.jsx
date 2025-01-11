8f8af4ceaffcaca7ca61f9cc602bf9c0c42c82a6
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-11 22:00:36 +0530

    added history, execution, result sections

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -2,9 +2,80 @@ import React from "react";
 import "./footer.css";
 
 function Footer() {
+  const keys = [
+    {
+      label: "1",
+      keyCode: 49,
+    },
+    {
+      label: "2",
+      keyCode: 50,
+    },
+    {
+      label: "3",
+      keyCode: 51,
+    },
+    {
+      label: "4",
+      keyCode: 52,
+    },
+    {
+      label: "5",
+      keyCode: 53,
+    },
+    {
+      label: "6",
+      keyCode: 54,
+    },
+    {
+      label: "7",
+      keyCode: 55,
+    },
+    {
+      label: "8",
+      keyCode: 56,
+    },
+    {
+      label: "9",
+      keyCode: 57,
+    },
+  ];
+  const symbols = [
+    {
+      symbols: "<=",
+      value: "backspace",
+      keyCode: 8,
+    },
+    {
+      symbols: "/",
+      value: "/",
+      keyCode: 191,
+    },
+    {
+      symbols: "x",
+      value: "*",
+      keyCode: 106,
+    },
+    {
+      symbols: "-",
+      value: "-",
+      keyCode: 189,
+    },
+    {
+      symbols: "+",
+      value: "+",
+      keyCode: 107,
+    },
+  ];
+
   return (
     <div className="footer">
-      <h1>footer</h1>
+      <div className="keys">
+        {keys.map((ele, ind) => {
+          <p key={ind}>{ele.label}</p>;
+        })}
+      </div>
+      <div className="symbols"></div>
     </div>
   );
 }
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,8 +3,33 @@ import "./header.css";
 
 function Header() {
   return (
-    <div className="header">
-      <h1>header</h1>
+    <div className="header custom-scroll">
+      <div className="history ">
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+        <p>3+4</p>
+      </div>
+      <div className="execution">
+        <p>32+2334+23+123*57678+32+2334+23</p>
+      </div>
+      <div className="result">
+        <p>9999999999999</p>
+      </div>
     </div>
   );
 }
